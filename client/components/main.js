3162b6804024f1ec1f528799c36cf8fd1b10c02d
Caitlin Trussell <caitlintrussell@gmail.com>
2018-07-15 09:29:10 -0500

    fixed styling

--- client/components/main.js
+++ client/components/main.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import styles from './index.css';
 import {SelectCohort, ChooseProject, SelectTeam, SelectView} from './index';
@@ -8,8 +8,10 @@ const Main = (props) => {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
+      <div className={styles.choiceWrapper}>
         <div className={styles.title}>Select Cohort</div>
           <SelectCohort />
+      </div>
 
         {!!selectedCohort.id &&
         <div className={styles.choiceWrapper}>
