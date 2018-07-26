edb1b8b13296e6f55ad9a8b3cf7cff54b64b0771
Cody Fayolle <34354074+cfayolle@users.noreply.github.com>
2018-07-26 15:14:34 -0500

    Update google.js

--- server/auth/google.js
+++ server/auth/google.js
@@ -19,7 +19,6 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
       const googleId = profile.id;
       const name = profile.displayName;
       const email = profile.emails[0].value;
-      console.log(profile);
       User.findOrCreate({
         where: {googleId},
         defaults: {name, email}
